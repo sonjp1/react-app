30e04a2ad4c2a8b2fa37b86c953e039a28f525f8
JP <sonjp1@idnsoft.co.kr>
2020-02-20 00:02:12 +0900

    Component Event Add

--- src/App.js
+++ src/App.js
@@ -8,19 +8,61 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            subject : {title : 'WEB', sub: 'World Wide Web'}
+            mode  : 'read',
+            selected_content_id : 2,
+            subject : {title : 'WEB', sub: 'World Wide Web'},
+            welcome : {title:'Welcome', desc: 'Hello, React!'},
+            contents : [
+                {id : 1, title : 'HTML', desc : 'HTML is for information'},
+                {id : 2, title : 'CSS', desc : 'CSS is for design'},
+                {id : 3, title : 'Javascript', desc : 'Javascript is for interactive'}
+            ]
         }
     }
 
     render() {
+        console.log('App render');
+
+        var _title, _desc = null;
+        if (this.state.mode === 'welcome') {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        }
+        else if (this.state.mode === 'read') {
+            var i = 0;
+            while(i < this.state.contents.length) {
+                var data = this.state.contents[i];
+                if (data.id === this.state.selected_content_id) {
+                    _title = data.title;
+                    _desc = data.desc;
+                    break;
+                }
+                i = i+1;
+            }
+        }
+
+        console.log('render', this);
+
         return(
             <div className="App">
                 <Subject
                     title={this.state.subject.title}
-                    sub={this.state.subject.sub}/>
-                <Subject title="React" sub="For UI" />
-                <TOC/>
-                <Content title="HTML" desc="HTML is HyperText Markup Language!"/>
+                    sub={this.state.subject.sub}
+                    onChangePage={function() {
+                        this.setState({mode:'welcome'});
+                    }.bind(this)}
+                />
+                <TOC
+                    data={this.state.contents}
+                    onChangePage={function(id) {
+                        this.setState({
+                            mode:'read',
+                            selected_content_id:Number(id)
+                        });
+
+                    }.bind(this)}
+                />
+                <Content title={_title} desc={_desc}/>
             </div>
         );
     }
--- src/components/Toc.js
+++ src/components/Toc.js
@@ -3,12 +3,32 @@ import Subject from "./Subject";
 
 class TOC extends Component {
     render() {
+        console.log('Toc render');
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+
+        while (i < data.length) {
+            lists.push(
+                <li key={data[i].id}>
+                    <a
+                        href={"/content/" + data[i].id}
+                        onClick={function(id, e) {
+                            e.preventDefault();
+                            this.props.onChangePage(id);
+                        }.bind(this, data[i].id)}
+                    >
+                        {data[i].title}
+                    </a>
+                </li>
+            );
+            i = i + 1;
+        }
+
         return(
             <nav>
                 <ul>
-                    <li><a href="1.html">HTML</a></li>
-                    <li><a href="1.html">CSS</a></li>
-                    <li><a href="1.html">Javascript</a></li>
+                    {lists}
                 </ul>
             </nav>
         );
